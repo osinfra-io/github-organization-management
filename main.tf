87907a28dbc0a8fd354921a37bc861725aeae9a5
Brett Curtis <brett@osinfra.io>
2023-01-11 19:56:45 -0500

    Ensure GitHub branch protection requires CODEOWNER reviews (#59)

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "github_branch_protection" "this" {
 
   required_pull_request_reviews {
     dismiss_stale_reviews           = true
-    require_code_owner_reviews      = false
+    require_code_owner_reviews      = true
     required_approving_review_count = 1
   }
 }
